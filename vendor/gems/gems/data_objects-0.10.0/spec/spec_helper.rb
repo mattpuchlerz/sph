a9b3eb0430b17e135c9c1386e21f828bcd6a75b2
Matt Puchlerz <matt@puchlerz.com>
2009-12-18 14:18:54 -0600

    Removed gem stuff that should be there.

--- vendor/gems/gems/data_objects-0.10.0/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'spec'
-
-dir = File.dirname(__FILE__)
-lib_path = File.expand_path("#{dir}/../lib")
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
-require 'data_objects'
-
-require File.expand_path(File.join(File.dirname(__FILE__), 'do_mock'))
--- vendor/gems/gems/do_sqlite3-0.10.0/README.markdown
+++
@@ -1,4 +0,0 @@
-do_sqlite3
-==========
-
-A DataObjects driver for SQLite3
